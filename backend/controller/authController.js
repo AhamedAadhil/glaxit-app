a4864892dd87e48c9d1557e755f1698ed4dbcdde
Ahamed Aathil <ahamedaathil.5@gmail.com>
2025-01-15 10:59:06 +0500

    hghgg

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { connectDB } from "../config/db.js";
-
+// dcdcd
 export const login = (req, res) => {
   // get user email from request body
   const { email } = req.body;
